5984515217932ef3b3348847c49c3b092e40ca21
jkuya <kuyajesee@proton.me>
2025-05-27 20:41:24 +0300

    Refactor Logger struct to use configurable output writer

--- logger/logger.go
+++ logger/logger.go
@@ -5,9 +5,8 @@ import (
 	"log"
 )
 
-// Logger handles download-related logging with configurable output
 type Logger struct {
-	writer *log.Logger // Logger instance with io.Writer
+    Output io.Writer 
 }
 
 // NewLogger creates a new Logger instance with the specified writer
