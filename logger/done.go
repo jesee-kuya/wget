68e3eb4acadd9f128c9690e8bb95e936dc1edf30
jkuya <kuyajesee@proton.me>
2025-05-27 21:46:08 +0300

    feat(logger): update Done method to log output to specified writer

--- logger/done.go
+++ logger/done.go
@@ -5,13 +5,10 @@ import (
 	"time"
 )
 
-type Logger struct{}
-
 // Done logs the completion of a download operation.
 // It outputs the final URL and the timestamp at which the download finished,
 // formatted as "yyyy-mm-dd hh:mm:ss" for consistency with wget-style logging.
-
 func (l *Logger) Done(timestamp time.Time, url string) {
-	fmt.Printf("Downloaded [%s]\n", url)
-	fmt.Printf("finished at %s\n", timestamp.Format("2006-01-02 15:04:05"))
+	fmt.Fprintf(l.Output, "\nDownloaded [%s]\n", url)
+	fmt.Fprintf(l.Output, "finished at %s\n", timestamp.Format("2006-01-02 15:04:05"))
 }
