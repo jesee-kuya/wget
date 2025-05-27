996bf53b1ae2072d8b0317613f5a06bc93dd5341
jkuya <kuyajesee@proton.me>
2025-05-27 21:45:22 +0300

    feat(logger): enhance Start method to log request status and format output

--- logger/start.go
+++ logger/start.go
@@ -5,11 +5,8 @@ import (
 	"time"
 )
 
-type Logger struct{}
-
 // Start logs the start of a process with a timestamp.
 func (l *Logger) Start(url string, timestamp time.Time) {
-	fmt.Printf("start at %s %s\n", timestamp.Format("2006-01-02 15:04:05"))
-	// fmt.Printf("sending request, awaiting response... %s\n", url)
-
+	fmt.Fprintf(l.Output, "start at %s\n", timestamp.Format("2006-01-02 15:04:05"))
+	fmt.Fprintf(l.Output, "sending request, awaiting response... ")
 }
