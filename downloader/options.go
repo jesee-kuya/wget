40e630fb184db2fcc328f793bc9ca0ef0fa3373e
jkuya <kuyajesee@proton.me>
2025-05-27 22:26:57 +0300

    feat(downloader): add Options struct for downloader configuration

---
+++ downloader/options.go
@@ -0,0 +1,11 @@
+package downloader
+
+// Options holds configuration flags passed to the downloader
+type Options struct {
+	OutputName  string  // -O: custom filename
+	OutputDir   string  // -P: directory to save file in
+	InputFile   string  // -i: input file with URLs
+	RateLimit   float64 // --rate-limit: in bytes per second
+	RunInBg     bool    // -B: download in background
+	LogFilePath string  // if -B is set, logs are redirected here
+}
