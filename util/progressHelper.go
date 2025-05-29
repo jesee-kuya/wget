26568622feac39b249388fff8ce56146c10ea24b
jkuya <kuyajesee@proton.me>
2025-05-30 00:50:40 +0300

    feat: add utility functions for formatting ETA and speed

---
+++ util/progressHelper.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"fmt"
+	"time"
+)
+
+// FormatETA formats the estimated time of arrival (ETA) in a human-readable format.
+func FormatETA(eta time.Duration) string {
+	if eta < 0 {
+		return "??s"
+	}
+	secs := int(eta.Seconds())
+	if secs < 60 {
+		return fmt.Sprintf("%ds", secs)
+	}
+	mins := secs / 60
+	secs = secs % 60
+	return fmt.Sprintf("%dm%ds", mins, secs)
+}
+
+// FormatSpeed formats the speed in a human-readable format.
+func FormatSpeed(bytesPerSec float64) string {
+	if bytesPerSec >= 1024*1024 {
+		return fmt.Sprintf("%.2f MiB/s", bytesPerSec/(1024*1024))
+	}
+	if bytesPerSec >= 1024 {
+		return fmt.Sprintf("%.2f KiB/s", bytesPerSec/1024)
+	}
+	return fmt.Sprintf("%.2f B/s", bytesPerSec)
+}
