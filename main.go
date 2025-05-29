b55aee1ca229de450d95f16dbee855d57075d3b8
jkuya <kuyajesee@proton.me>
2025-05-30 02:19:20 +0300

    feat(main): add background download option with logging to wget-log

--- main.go
+++ main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/jesee-kuya/wget/downloader"
 	"github.com/jesee-kuya/wget/logger"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Usage: go run . <url>")
+	background := flag.Bool("B", false, "Download in background and log output to wget-log")
+
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) == 0 {
+		fmt.Println("Usage: wget [options] <URL>")
 		return
 	}
-	url := os.Args[1]
+
+	url := args[0]
+
+	if *background {
+		fmt.Println("Output will be written to \"wget-log\".")
+		cmdArgs := append([]string{"wget"}, args...)
+		cmd := exec.Command(os.Args[0], cmdArgs...)
+
+		logFile, err := os.Create("wget-log")
+		if err != nil {
+			fmt.Println("Error creating log file:", err)
+			return
+		}
+		defer logFile.Close()
+		cmd.Stdout = logFile
+		cmd.Stderr = logFile
+		err = cmd.Start()
+		if err != nil {
+			fmt.Println("Failed to start background download:", err)
+			return
+		}
+		return
+	}
 
 	log := logger.NewLogger(os.Stdout)
 
