033f8f6fb06b2aa252edc700f0711812084a3095
jkuya <kuyajesee@proton.me>
2025-05-30 01:17:48 +0300

    refactor(main): update main function to handle errors gracefully without exiting

--- main.go
+++ main.go
@@ -11,23 +11,20 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "Usage: go run . <url>")
-		os.Exit(1)
+		return
 	}
 	url := os.Args[1]
 
-	// Set up logger
 	log := logger.NewLogger(os.Stdout)
 
-	// Use default options (no output name or directory)
 	opts := downloader.Options{
 		OutputName: "",
 		OutputDir:  "",
 	}
 
-	// Run the download
 	err := downloader.DownloadFile(url, opts, log)
 	if err != nil {
 		log.Error(err)
-		os.Exit(1)
+		return
 	}
 }
