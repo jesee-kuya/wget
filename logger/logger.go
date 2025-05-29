82c972ef55a56888032effab0091a5e7ef1a75f3
Tomlee Abila <tomleeabila@gmail.com>
2025-05-29 06:33:30 +0300

    feat: content size refiniment

--- logger/logger.go
+++ logger/logger.go
@@ -35,19 +35,27 @@ func (l *Logger) SavingTo(path string) {
 
 // ContentInfo logs the size of the content being downloaded.
 func (l *Logger) ContentInfo(size int64) {
+	readable := contentSize(size)
+	fmt.Fprintf(l.Output, "content size: %d [~%s]\n", size, readable)
+}
+
+//ContentSize converts size of the content into GB or MB
+func contentSize(size int64) string{
 	const (
-		MB = 1024.0 * 1024.0
-		GB = 1024.0 * 1024.0 * 1024.0
+		KiB = 1024.0
+		MiB = 1024.0 * KiB
+		GiB = 1024.0 * MiB
 	)
-
-	var readable string
-	if float64(size) >= GB {
-		readable = fmt.Sprintf("~%.2fGB", float64(size)/GB)
-	} else {
-		readable = fmt.Sprintf("~%.2fMB", float64(size)/MB)
+	
+	if float64(size) >= GiB {
+		return fmt.Sprintf("%.2fGiB", float64(size)/GiB)
+	} else if float64(size) >= MiB{
+		return fmt.Sprintf("%.2fMiB", float64(size)/MiB)
+	}else if float64(size) >= KiB{
+		return fmt.Sprintf("%.2fKiB", float64(size)/KiB)
 	}
-
-	fmt.Fprintf(l.Output, "content size: %d [%s]\n", size, readable)
+	
+	return fmt.Sprint("%.2fB", float64(size))
 }
 
 //Output the status code of the process
@@ -57,6 +65,31 @@ func (l *Logger) Status(code int){
 		status = "Unknown Status"
 	}
 	fmt.Fprintf(l.Output, "sending request, awaiting response... status %d %s\n",code , status)
+}
+
+//Output the progress of download
+func (l *Logger) Progress(written, total int64, speed float64, eta time.Duration){
+	if written < 0{
+		written = 0;
+	}
+
+	if speed < 0{
+		speed = 0;
+	}
+
+	percentage := (float64(written)/float64(total))*100.0
+}
+
+func etaFormat(eta time.Duration) string{
+	if eta < time.Second{
+		return "0s"
+	}
+
+	seconds := int64(eta.Seconds())
+	if seconds < 60 {
+		return fmt.Sprintf("%ds", seconds)
+	}
+	return (time.Duration(seconds) * time.Second).String()
 }
 
 // Error logs an error message.
