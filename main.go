e73c7afee53c2cea74743cdff486e7b6046fb941
jkuya <kuyajesee@proton.me>
2025-05-30 01:15:37 +0300

    feat: implement main function to handle URL input and initiate download

--- main.go
+++ main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/jesee-kuya/wget/downloader"
+	"github.com/jesee-kuya/wget/logger"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Usage: go run . <url>")
+		os.Exit(1)
+	}
+	url := os.Args[1]
+
+	// Set up logger
+	log := logger.NewLogger(os.Stdout)
+
+	// Use default options (no output name or directory)
+	opts := downloader.Options{
+		OutputName: "",
+		OutputDir:  "",
+	}
+
+	// Run the download
+	err := downloader.DownloadFile(url, opts, log)
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+}
