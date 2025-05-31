cce831c2db776acc2aa9ed4f4032af5ca09e3f20
jkuya <kuyajesee@proton.me>
2025-06-01 02:31:44 +0300

    feat: Implement background execution for wget with logging to file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/jesee-kuya/wget/downloader"
 	"github.com/jesee-kuya/wget/logger"
@@ -41,7 +42,40 @@ func main() {
 	if *background {
 		fmt.Println("Output will be written to \"wget-log\".")
 
-		logFile, err := os.Create("wget-log")
+		// If already running in background, skip re-exec
+		if os.Getenv("WGET_BACKGROUND") != "1" {
+			// Re-exec the same binary in background
+			execPath, err := os.Executable()
+			if err != nil {
+				fmt.Println("Error finding executable path:", err)
+				return
+			}
+
+			cmd := exec.Command(execPath, os.Args[1:]...)
+			cmd.Env = append(os.Environ(), "WGET_BACKGROUND=1")
+
+			// Redirect output to log file
+			logFile, err := os.OpenFile("wget-log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
+			if err != nil {
+				fmt.Println("Error creating log file:", err)
+				return
+			}
+			defer logFile.Close()
+
+			cmd.Stdout = logFile
+			cmd.Stderr = logFile
+			cmd.Stdin = nil // disconnect input
+
+			// Start the process and exit the parent
+			if err := cmd.Start(); err != nil {
+				fmt.Println("Failed to start background process:", err)
+				return
+			}
+			return
+		}
+
+		// Actual background logic starts here
+		logFile, err := os.OpenFile("wget-log", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o644)
 		if err != nil {
 			fmt.Println("Error creating log file:", err)
 			return
@@ -50,7 +84,6 @@ func main() {
 
 		fileLogger := logger.NewLogger(logFile)
 
-		// Perform the download using our downloader
 		err = downloader.DownloadFile(url, opts, fileLogger)
 		if err != nil {
 			fmt.Fprintf(logFile, "Download failed: %v\n", err)
