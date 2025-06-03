bb7c14cc5d5e4d9088d1bdda60b71e6eb305e540
jkuya <kuyajesee@proton.me>
2025-06-03 09:18:49 +0300

    feat: enhance ExtractLinks function to recursively traverse HTML nodes and extract asset URLs

--- parser/parser.go
+++ parser/parser.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"io"
 	"net/url"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -18,5 +19,42 @@ func ExtractLinks(baseURL *url.URL, r io.Reader) ([]string, error) {
 	var links []string
 	visited := make(map[string]bool)
 
+	var traverse func(*html.Node)
+	traverse = func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			var key string
+			switch n.Data {
+			case "a", "link":
+				key = "href"
+			case "img":
+				key = "src"
+			}
+
+			if key != "" {
+				for _, attr := range n.Attr {
+					if attr.Key == key {
+						href := strings.TrimSpace(attr.Val)
+						if href == "" {
+							continue
+						}
+						u, err := baseURL.Parse(href)
+						if err == nil && u.Host == baseURL.Host {
+							cleanURL := u.String()
+							if !visited[cleanURL] {
+								visited[cleanURL] = true
+								links = append(links, cleanURL)
+							}
+						}
+					}
+				}
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			traverse(c)
+		}
+	}
+
+	traverse(doc)
 	return links, nil
 }
