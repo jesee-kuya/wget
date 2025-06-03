5871bf89aed88b434d92fef41f3d0ade8c4a9fdb
jkuya <kuyajesee@proton.me>
2025-06-03 09:18:29 +0300

    feat: implement ExtractLinks function to parse HTML and extract internal asset URLs

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,22 @@
 package parser
+
+import (
+	"io"
+	"net/url"
+
+	"golang.org/x/net/html"
+)
+
+// ExtractLinks parses HTML from the reader and returns internal asset URLs.
+// It extracts href/src from <a>, <link>, and <img> tags.
+func ExtractLinks(baseURL *url.URL, r io.Reader) ([]string, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var links []string
+	visited := make(map[string]bool)
+
+	return links, nil
+}
