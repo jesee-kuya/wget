f16bb2f109e050c23cc22a2f2d23ff977b5575e1
jkuya <kuyajesee@proton.me>
2025-06-05 02:16:55 +0300

    feat: add options to reject file suffixes and exclude directory paths in downloader

--- downloader/options.go
+++ downloader/options.go
@@ -2,10 +2,12 @@ package downloader
 
 // Options holds configuration flags passed to the downloader
 type Options struct {
-	OutputName  string  // -O: custom filename
-	OutputDir   string  // -P: directory to save file in
-	InputFile   string  // -i: input file with URLs
-	RateLimit   float64 // --rate-limit: in bytes per second
-	RunInBg     bool    // -B: download in background
-	LogFilePath string  // if -B is set, logs are redirected here
+	OutputName  string   // -O: custom filename
+	OutputDir   string   // -P: directory to save file in
+	InputFile   string   // -i: input file with URLs
+	RateLimit   float64  // --rate-limit: in bytes per second
+	RunInBg     bool     // -B: download in background
+	LogFilePath string   // if -B is set, logs are redirected here
+	Reject      []string // -R: file suffixes to skip (e.g. []string{"jpg","gif"})
+	Exclude     []string // -X: directory paths to skip (e.g. []string{"/js","/assets"})
 }
