3ecfad6dd859946de048adb79bb1ed7b5c00212b
Franklyne <112707386+fnamayi@users.noreply.github.com>
2025-06-06 03:38:44 +0300

    Cleaned up redundant logs in Start() and Status()

--- logger/logger.go
+++ logger/logger.go
@@ -29,7 +29,7 @@ func (l *Logger) Done(timestamp time.Time, url string) {
 // Start logs the start of a process with a timestamp.
 func (l *Logger) Start(url string, timestamp time.Time) {
 	fmt.Fprintf(l.Output, "start at %s\n", timestamp.Format("2006-01-02 15:04:05"))
-	fmt.Fprintf(l.Output, "sending request, awaiting response... ")
+	//fmt.Fprintf(l.Output, "sending request, awaiting response... ")
 }
 
 // SavingTo logs the path where the file is being saved.
